21e172cef3c0e52128d332afb9668fdf8cb14c8c
humberto geovanni marinao vidal <humberto.mv@gmail.com>
2019-07-24 12:50:35 -0400

    Carga Inicial

---
+++ Devops_Ennovate.BLL/Metodo.vb
@@ -0,0 +1,8 @@
+﻿Public Class Metodo
+    Public Shared Function MostrarConneccion()
+        Dim a As String
+        a = Devops_Ennovate.DAL.Conexion.Coneccion
+        Return a
+    End Function
+
+End Class
---
+++ Devops_Ennovate.DAL/Conexion.vb
@@ -0,0 +1,5 @@
+﻿Public Class Conexion
+
+    Public Shared Coneccion = "coneccion a base de datos se realizo correctamente"
+
+End Class
